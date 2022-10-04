a4a03eba46f76c2345aca5878da9f76b5fbc6fa3
Mike Menne <mike.menne@gmail.com>
2022-10-04 15:54:12 -0500

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# make a dataframe from json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display it
+streamlit.dataframe(fruityvice_normalized)
